de77c4c12fb27287263a700f3df8982f4697fe71
MdISBAULHAQUE <mdisbaulh@gmail.com>
2025-04-22 22:18:07 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Nav from "./component/Nav/Nav";
 import Home from "./component/Home/Home";
 import About from "./component/About/About";
 import Project from "./component/Project/Project";
-import Contact from "./component/Contact/Contact";
+import Contact from "./component/contact/Contact";
 
 import { useState } from "react";
 
