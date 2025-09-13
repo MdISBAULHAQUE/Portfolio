a4d9b6b2595585377301cda263242b63810fa492
MdISBAULHAQUE <mdisbaulh@gmail.com>
2025-09-13 11:30:16 +0530

    upadte new

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import "./About.css"
-import Card from '../card/Card'
-import mern from "../../../picture/mern.png"
-import java from "../../../picture/java.png"
-import dsa from "../../../picture/dsa.png"
+import React from "react";
+import "./About.css";
+import Card from "../card/Card";
+import mern from "../../../picture/mern.png";
+import java from "../../../picture/java.png";
+import dsa from "../../../picture/dsa.png";
 const About = () => {
   return (
     <div id="about">
@@ -20,16 +20,16 @@ const About = () => {
             <h1>Personal Info</h1>
             <ul>
               <li>
-                <span>NAME</span> : MD ISBAUL HAQUE 
+                <span>NAME</span> : MD ISBAUL HAQUE
               </li>
               <li>
                 <span>AGE</span> : 22
               </li>
               <li>
-                <span>GENDER</span> : MALE 
+                <span>GENDER</span> : MALE
               </li>
               <li>
-                <span>LANGUAGE</span> : BENGALI ,  ENGLISH ,HINDI
+                <span>LANGUAGE</span> : BENGALI , ENGLISH ,HINDI
               </li>
             </ul>
           </div>
@@ -37,7 +37,7 @@ const About = () => {
             <h1>Education</h1>
             <ul>
               <li>
-                <span>DEGREE</span> : B-TECH 
+                <span>DEGREE</span> : B-TECH
               </li>
               <li>
                 <span>BRANCH</span> : COMPUTER SCIENCE & ENGINEERING
@@ -50,26 +50,21 @@ const About = () => {
           <div className="skills">
             <h1>Skills</h1>
             <ul>
-              <li>
-                MERN STACK WEB DEVELOPER
-              </li>
-              <li>
-                DSA
-              </li>
-              <li>
-                JAVA
-              </li>
+              <li>MERN STACK WEB DEVELOPER</li>
+              <li>Machine Learning </li>
+              <li>DSA</li>
+              <li>JAVA</li>
             </ul>
           </div>
         </div>
       </div>
       <div className="rightabout">
-        <Card title="MERN STACK WEB DEVELOPER" image={mern}/>
-        <Card title="JAVA" image={java}/>
-        <Card title="DSA" image={dsa}/>
+        <Card title="MERN STACK WEB DEVELOPER" image={mern} />
+        <Card title="JAVA" image={java} />
+        <Card title="DSA" image={dsa} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/component/Project/Project.jsx
+++ src/component/Project/Project.jsx
@@ -4,8 +4,8 @@ import Card from "../card/Card";
 import aiweb from "../../../picture/fw.png";
 import va from "../../../picture/va.png";
 import tti from "../../../picture/tti.png";
-import m from "../../../picture/medical.jpeg"
-import task from"../../../picture/pic-banner.webp"
+import m from "../../../picture/medical.jpeg";
+import task from "../../../picture/pic-banner.webp";
 
 const Project = () => {
   return (
@@ -13,8 +13,12 @@ const Project = () => {
       <div className="slider">
         <Card title="MediConnect" image={m} />
         <Card title="Task-flow" image={task} />
-        <Card  title ="AI Assistant" image={va} onClick={() => window.open('https://zita-ai.netlify.app/', '_blank')} />
-        <Card title=" AI Text To Images" image={tti} />
+        <Card
+          title="AI Assistant"
+          image={va}
+          onClick={() => window.open("https://zita-ai.netlify.app/", "_blank")}
+        />
+        <Card title=" AI Text To Images" image={tti} onClick={() => window.open('https://textureai.netlify.app/', '_blank')} />
       </div>
     </div>
   );
